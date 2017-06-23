e63c3d7dbfbc0f3dec93737c0c99e1680c183563
ajesler <andrew.esler@powershop.co.nz>
2017-06-23 12:50:55 +1200

    Add dry run option

--- lib/ciq/cli.rb
+++ lib/ciq/cli.rb
@@ -5,6 +5,7 @@ require "thor"
 module Ciq
   class CLI < Thor
     class_option :verbose, type: :boolean
+    class_option :dry_run, type: :boolean, desc: "Print the ConnectIQ commands that would be run, but don't run any"
 
     desc "manifest", "Display information from the manifest"
     option :file, default: "manifest.xml", desc: "The path to a ConnecIQ manifest file"
